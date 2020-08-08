a732991036a7168022b66266df0f49a5424b4c8f
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-08 02:25:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ end
 
 def kesha_maker (array)
   $_replace = []
-  array.each_with_index do |character,2|
+  array.each_with_index do |character,index|
     $_replace << ("character[index(2)] = $")
   end
   $_replace
